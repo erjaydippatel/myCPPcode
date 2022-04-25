0503763d3cdb39850bbb0bf76f0b46d2b7ae265a
unknown <jaydip.patel@seltec.io>
2022-04-25 10:44:34 +0530

    first commit

---
+++ Function_Overloading/Function_Overloading/Function_Overloading.mk
@@ -0,0 +1,114 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Release
+ProjectName            :=Function_Overloading
+ConfigurationName      :=Release
+WorkspaceConfiguration :=Release
+WorkspacePath          :=D:/CPP_workspace/CPP_workspace
+ProjectPath            :="C:/Users/admin/Desktop/How to Study Best/CPP/github/code/myCPPcode/Function_Overloading/Function_Overloading"
+IntermediateDirectory  :=/CPP_workspace/CPP_workspace/build-$(WorkspaceConfiguration)/Users/admin/Desktop/How_to_Study_Best/CPP/github/code/myCPPcode/Function_Overloading/Function_Overloading
+OutDir                 :=$(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=admin
+Date                   :=25/04/2022
+CodeLitePath           :=D:/CodeLite
+MakeDirCommand         :=mkdir
+LinkerName             :=C:/MinGW/bin/g++.exe
+SharedObjectLinkerName :=C:/MinGW/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputDirectory        :=D:/CPP_workspace/CPP_workspace/build-$(WorkspaceConfiguration)/bin
+OutputFile             :=D:\CPP_workspace\CPP_workspace\build-$(WorkspaceConfiguration)\bin\$(ProjectName).exe
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :=$(IntermediateDirectory)/ObjectsList.txt
+PCHCompileFlags        :=
+RcCmpOptions           := 
+RcCompilerName         :=C:/MinGW/bin/windres.exe
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overridden using an environment variable
+##
+AR       := C:/MinGW/bin/ar.exe rcu
+CXX      := C:/MinGW/bin/g++.exe
+CC       := C:/MinGW/bin/gcc.exe
+CXXFLAGS := -std=c++17 -Wall -O2 -std=c++17 -Wall $(Preprocessors)
+CFLAGS   :=  -O2 -std=c++17 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := C:/MinGW/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=D:\CodeLite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: MakeIntermediateDirs $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@if not exist "$(IntermediateDirectory)" $(MakeDirCommand) "$(IntermediateDirectory)"
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@if not exist "$(IntermediateDirectory)" $(MakeDirCommand) "$(IntermediateDirectory)"
+	@if not exist "$(OutputDirectory)" $(MakeDirCommand) "$(OutputDirectory)"
+
+$(IntermediateDirectory)/.d:
+	@if not exist "$(IntermediateDirectory)" $(MakeDirCommand) "$(IntermediateDirectory)"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/admin/Desktop/How to Study Best/CPP/github/code/myCPPcode/Function_Overloading/Function_Overloading/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
+
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r $(IntermediateDirectory)
+
+
